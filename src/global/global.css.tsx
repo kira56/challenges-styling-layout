b4de39fd20e6c98f45db927bd582c9d0619b536f
Diego Cahuayo <diegocahuayo@Diegos-MacBook-Pro.local>
2022-10-07 16:51:24 -0500

    feat: Create and config global css of styled components

---
+++ src/global/global.css.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    body{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+`;
