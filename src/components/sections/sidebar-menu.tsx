4899b0f9c30163e197a9ae5d9db928d23020ad74
Diego Cahuayo <diegocahuayo@Diegos-MacBook-Pro.local>
2022-10-08 15:45:50 -0500

    feat: Creeate Sidebar menu and import on Sidebar component

---
+++ src/components/sections/sidebar-menu.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import { BoxStyled } from "../snippets/menu-item";
+
+const SidebarMenu = () => {
+  return (
+    <SidebarNav>
+      <SidebarNavList>
+        <SidebarNavItem>
+          <BoxStyled />
+          <BoxStyled />
+        </SidebarNavItem>
+        <SidebarNavItem>
+          Name <BoxUser />
+        </SidebarNavItem>
+      </SidebarNavList>
+    </SidebarNav>
+  );
+};
+
+export default SidebarMenu;
+
+const SidebarNav = styled.nav`
+  list-style: none;
+  padding: 5px 30px;
+  border-bottom: 1px solid #e0e7f1;
+`;
+
+const SidebarNavList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  padding-inline-start: 0;
+`;
+
+const SidebarNavItem = styled.li`
+  list-style: none;
+  display: flex;
+  align-items: center;
+`;
+
+const BoxUser = styled.div`
+  width: 31px;
+  height: 31px;
+  background-color: #c4c4c4;
+  border-radius: 50%;
+  margin-left: 10px;
+`;
--- src/components/sections/sidebar.tsx
+++ src/components/sections/sidebar.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import CardInfo from "../snippets/card/card-info";
+import SidebarMenu from "./sidebar-menu";
 
 const Sidebar = () => {
-  return <Aside>Sidebar</Aside>;
+  // Add vertical properties in this component
+  return (
+    <Aside>
+      <SidebarMenu />
+      <AsideBottom>
+        <CardInfo />
+      </AsideBottom>
+    </Aside>
+  );
 };
 
 export default Sidebar;
@@ -11,4 +21,11 @@ const Aside = styled.aside`
   background-color: #f5f6fc;
   grid-area: sidebar;
   color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const AsideBottom = styled.div`
+  padding: 20px 33px;
 `;
