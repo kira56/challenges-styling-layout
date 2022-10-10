9a55c6660b1d6db8d6c7047460d5a4be2e8182c0
Diego Cahuayo <diegocahuayo@Diegos-MacBook-Pro.local>
2022-10-10 16:24:55 -0500

    chore: Add tags and reset properties

--- src/global/global.css.tsx
+++ src/global/global.css.tsx
@@ -7,4 +7,20 @@ export default createGlobalStyle`
         box-sizing: border-box;
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
+
+    h1,h2,h3,h4,h5 {
+        margin-block-start: 0;
+        margin-block-end: 0;
+    }
+
+    p {
+        margin-block-start: 0;
+        margin-block-end: 0;
+    }
+
+    ul{
+        padding-inline-start: 0;
+        list-style: none;
+        text-align: start;
+    }
 `;
