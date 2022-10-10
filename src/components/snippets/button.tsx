8f731374de5ae062f3972a017dc8c215f6d15b14
Diego Cahuayo <diegocahuayo@Diegos-MacBook-Pro.local>
2022-10-10 16:17:57 -0500

    chore: Update button component and add interface

--- src/components/snippets/button.tsx
+++ src/components/snippets/button.tsx
@@ -1,27 +1,59 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
-const Button = () => {
+interface Props {
+  text: string;
+  plus?: boolean;
+}
+
+const Button = ({ text, plus = false }: Props) => {
   return (
-    <ButtonStyled>
-      <button>Create New</button>
+    <ButtonStyled plus={plus}>
+      <button>
+        {text}
+        {plus && (
+          <BoxPlus>
+            <FontAwesomeIcon icon={faPlus} />
+          </BoxPlus>
+        )}
+      </button>
     </ButtonStyled>
   );
 };
 
 export default Button;
 
-const ButtonStyled = styled.div`
+const ButtonStyled = styled.div<Omit<Props, "text">>`
   background-color: #ff9f00;
+  height: 45px;
   border-radius: 7px;
+  padding-left: ${(props) => (props.plus ? "15px" : 0)};
+  cursor: pointer;
+
   button {
-    padding: 12px 0;
     color: #fff;
     background: transparent;
     border: none;
     width: 100%;
     height: 100%;
     font-size: 14px;
-    font-weight: 200;
+    font-weight: 500;
+    display: flex;
+    justify-content: ${(props) => (props.plus ? "space-between" : "center")};
+    align-items: center;
+    text-align: ${(props) => (props.plus ? "start" : "center")};
   }
+`;
+
+const BoxPlus = styled.div`
+  background-color: #ffb233;
+  width: 35px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 5px;
 `;
